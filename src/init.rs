c96221141637fc48ffa035a3a069f83f7675056c
Kosiachenko <dkosiac@cmtclm2dt07lmd6.rd.corpintra.net>
2024-01-24 15:28:20 +0100

    changed back to layers

--- src/init.rs
+++ src/init.rs
@@ -1,21 +1,22 @@
 use std::env;
 
-use tracing_appender::non_blocking::{NonBlocking, WorkerGuard};
-use tracing_subscriber::{EnvFilter, Layer, Registry};
-use tracing_subscriber::fmt::format::JsonFields;
-use tracing_subscriber::layer::{Layered, SubscriberExt};
+use tracing::Subscriber;
+use tracing_appender::non_blocking::WorkerGuard;
+use tracing_subscriber::{EnvFilter, Layer, registry};
 
 use crate::formatter::DatadogFormatter;
 use crate::shutdown::TracerShutdown;
 use crate::tracer::build_tracer;
 
-type RegistryLayer = Layered<Box<dyn Layer<Registry> + Send + Sync>, Registry>;
-type FormatterLayer = Layered<Box<tracing_subscriber::fmt::Layer<RegistryLayer, JsonFields, DatadogFormatter, NonBlocking>>, RegistryLayer>;
-type EnvFilterLayer = Layered<EnvFilter, FormatterLayer>;
+pub struct DatadogLayers<S> {
+    pub loglevel_layer: EnvFilter,
+    pub guard: WorkerGuard,
+    pub trace_shutdown: TracerShutdown,
+    pub telemetry_layer: Box<dyn Layer<S>>,
+    pub log_layer: Box<dyn Layer<S>>,
+}
 
-pub type RegisterType = Box<EnvFilterLayer>;
-
-pub fn init() -> anyhow::Result<(RegisterType, WorkerGuard, TracerShutdown)> {
+pub fn init<S>() -> Box<DatadogLayers<S>> where S: Subscriber + for<'a> registry::LookupSpan<'a> {
     let (non_blocking, guard) = tracing_appender::non_blocking(std::io::stdout());
     let dd_enabled = env::var("DD_ENABLED").map(|s| s == "true").unwrap_or_else(|_| false);
     let log_layer = Box::new(tracing_subscriber::fmt::layer()
@@ -24,15 +25,25 @@ pub fn init() -> anyhow::Result<(RegisterType, WorkerGuard, TracerShutdown)> {
         .with_writer(non_blocking));
     let loglevel_layer = loglevel_filter_layer(dd_enabled);
     if dd_enabled {
-        let tracer = build_tracer()?;
-        let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer).boxed();
-        let registry = Box::new(Registry::default()
-            .with(telemetry_layer)
-            .with(log_layer)
-            .with(loglevel_layer));
-        Ok((registry, guard, TracerShutdown {}))
+        let tracer = build_tracer().unwrap_or_else(|e| panic!("Could not init datadog tracer: {}", e));
+        let telemetry_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+        let telemetry_layer = Box::new(telemetry_layer);
+        let t = DatadogLayers {
+            log_layer,
+            telemetry_layer,
+            loglevel_layer,
+            guard,
+            trace_shutdown: TracerShutdown {},
+        };
+        Box::new(t)
     } else {
-        return Err(anyhow::anyhow!("DD_ENABLED is not set"));
+        Box::new(DatadogLayers {
+            log_layer,
+            telemetry_layer: Box::new(tracing_subscriber::fmt::layer()),
+            loglevel_layer: loglevel_layer,
+            guard,
+            trace_shutdown: TracerShutdown {},
+        })
     }
 }
 
